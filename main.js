6a7c542e006ad02b7bf254e103db37407ddb26c4
Anujknitdgp <77582166+Anujknitdgp@users.noreply.github.com>
2023-02-09 20:35:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+window.addEventListener('keydown', function (e) {
+ const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+ const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+ if (!audio) return;
+ audio.currentTime = 0;
+ audio.play();
+ key.classList.add('playing');
+
+ this.setTimeout(function () { key.classList.remove('playing'); }, 0700)
+
+})
